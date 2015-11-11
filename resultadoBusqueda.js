1ef4e8a8e1e29c9e970cb24e836504a68957e878
victorcruz94 <1000316.joan23@fje.edu>
2015-11-11 11:29:09 +0100

    JS del index2, css del index 2 y carpeta de imagenes

---
+++ resultadoBusqueda.js
@@ -0,0 +1,38 @@
+function resultado1(){
+	document.getElementById("formulario1").style.display="inline-block";
+	var formulario = document.getElementById("formulario");
+	var titulo = document.getElementById("titulo")
+	titulo.parentNode.removeChild(titulo);
+	var titulo = document.createElement("h2");
+	var contenido = document.createTextNode("Aules de Teoria");
+	titulo.appendChild(contenido);
+	formulario.appendChild(titulo);
+}
+function resultado2(){
+	document.getElementById("formulario1").style.display="inline-block";
+	var formulario = document.getElementById("formulario");
+	var titulo = document.getElementById("titulo")
+	titulo.parentNode.removeChild(titulo);
+	var titulo = document.createElement("h2");
+	var contenido = document.createTextNode("Aules d'Informàtica");
+	titulo.appendChild(contenido);
+	formulario.appendChild(titulo);
+}
+function resultado3(){
+	document.getElementById("formulario1").style.display="inline-block";
+}
+function resultado4(){
+	document.getElementById("formulario1").style.display="inline-block";
+}
+function resultado5(){
+	document.getElementById("formulario1").style.display="inline-block";
+}
+function resultado6(){
+	document.getElementById("formulario1").style.display="inline-block";
+}
+function resultado7(){
+	document.getElementById("formulario1").style.display="inline-block";
+}
+function resultado8(){
+	document.getElementById("formulario1").style.display="inline-block";
+}
